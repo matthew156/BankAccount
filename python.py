e7511c6cdc695145d91b52fba74c570806801493
matthew156 <matthewastro@gmail.com>
2021-08-31 16:51:00 -0400

    Bank Account 2nd Commit

--- python.py
+++ python.py
@@ -10,6 +10,9 @@ class Bankaccount:
         return self
     def withdraw(self, amount):
         self.balance -= amount
+        if self.balance < 0:
+            self.balance -= 5
+            print('Insufficient funds, charging a $5 fee...')
         return self    
     def display_account_info(self):
         print(f'Your Account Balance is: {self.balance}')
@@ -22,7 +25,12 @@ peter = Bankaccount('Peter', 'peter@fakeemail.com', 15, 1000)
 
 lois = Bankaccount('Lois', 'peter@fakeemail.com', 15, 1000)
 
+hank = Bankaccount('Hank', 'peter@fakeemail.com', 15, 100)
 
 peter.deposit(2000).deposit(100).deposit(100).withdraw(100).yield_interest().display_account_info()
 
 lois.deposit(2000).deposit(100).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yield_interest().display_account_info()
+
+
+hank.withdraw(105)
+hank.display_account_info()
